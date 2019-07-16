8217984080c75cfd4318e649742c6a7df630f347
Mark Chen <mchen@uts-canada.com>
2019-07-16 17:16:47 -0600

    import

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name deployAngularJstoGithubPagesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the deployAngularJstoGithubPagesApp
+ */
+angular.module('deployAngularJstoGithubPagesApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name deployAngularJstoGithubPagesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the deployAngularJstoGithubPagesApp
+ */
+angular.module('deployAngularJstoGithubPagesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
