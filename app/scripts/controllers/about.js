7ce0b28e1c7aa202261e24fb78824ac7e7cf484d
Mark Chen <mchen@uts-canada.com>
2019-07-17 11:22:34 -0600

    changes!

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -1,12 +1,5 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name deployAngularJstoGithubPagesApp.controller:AboutCtrl
- * @description
- * # AboutCtrl
- * Controller of the deployAngularJstoGithubPagesApp
- */
 angular.module('helloApp')
   .controller('AboutCtrl', function () {
     this.awesomeThings = [
