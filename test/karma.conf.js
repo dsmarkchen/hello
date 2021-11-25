e12531c0bad6e5f18cd61926ea50bcb5de29d2dd
Mark Chen <mchen@uts-canada.com>
2021-11-25 09:32:31 -0700

    bower update: move jquery.js up in index.html

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -21,6 +21,7 @@ module.exports = function(config) {
     files: [
       // bower:js
       'bower_components/angular/angular.js',
+      'bower_components/jquery/dist/jquery.js',
       'bower_components/bootstrap/dist/js/bootstrap.js',
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-cookies/angular-cookies.js',
@@ -28,7 +29,6 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
-      'bower_components/jquery/dist/jquery.js',
       'bower_components/showdown/dist/showdown.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
